1868665af518616194522a39d0bd82cde9eb720f
hani <itshanibee@gmail.com>
2023-10-15 21:16:03 +0530

    userscripts: add device specific stuff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+# Shelby's Bits
 git clone https://github.com/PixelExperience-Devices/device_qcom_common.git device/qcom/common
 git clone https://github.com/PixelExperience-Devices/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
 git clone https://github.com/PixelExperience-Devices/device_qcom_qssi.git device/qcom/qssi
@@ -7,3 +8,9 @@ git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_co
 git clone https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
 git clone https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
 git clone https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm
+
+# Vendor Blobs
+git clone https://github.com/itsHanibee/vendor_xiaomi_chime vendor/xiaomi/chime
+
+# Kernel
+git clone --depth=1 https://github.com/itsHanibee/kernel_xiaomi_chime -b main kernel/xiaomi/chime
