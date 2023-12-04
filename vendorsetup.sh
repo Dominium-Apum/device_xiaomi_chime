1a1c1c665ce6ec99dd45c090b19364a4fe116ed6
itsHanibee <itshanibee@gmail.com>
2023-12-04 07:20:36 +0000

    chime: update pull locations
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,12 @@
 # Clone Kernel
-git clone --depth=1 https://github.com/itsHanibee/kernel_xiaomi_chime -b android kernel/xiaomi/chime
+git clone --depth=1 https://github.com/Dominium-Apum/kernel_xiaomi_chime -b kenvyra-13 kernel/xiaomi/chime
 
 # Clone Vendor
-git clone https://github.com/BootleggersROM-Devices/vendor_xiaomi_chime vendor/xiaomi/chime
+git clone https://github.com/Dominium-Apum/vendor_xiaomi_chime vendor/xiaomi/chime
 
 # Clone Hardware
 # rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-
 
 export BUILD_USERNAME=hani
 export BUILD_HOSTNAME=dungeon
